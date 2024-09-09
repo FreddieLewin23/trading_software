17f8e07ef310d9935e0150839b354bbc01bd467c
FreddieLewin23 <142442045+FreddieLewin23@users.noreply.github.com>
2024-09-09 17:57:27 +0100

    Update scheduler.py
    changes to initial execution of algo

--- scheduler.py
+++ scheduler.py
@@ -6,7 +6,7 @@ import os
 import json
 import pandas as pd
 import qi_client
-import datetime
+from datetime import datetime, timedelta
 import csv
 import numpy as np
 import concurrent.futures
@@ -15,7 +15,6 @@ import warnings
 import math
 import threading
 import Qi_wrapper
-from backtest_json_redo_MVG import grab_data2, find_sd_from_model_data, subtract_days_from_date
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 os.environ['QI_API_KEY'] = ''
@@ -23,14 +22,17 @@ configuration = qi_client.Configuration()
 configuration.api_key['X-API-KEY'] = ''
 api_instance = qi_client.DefaultApi(qi_client.ApiClient(configuration))
 
+
 def job():
-    now = datetime.datetime.now()
+    now = datetime.now()
     if 14 <= now.hour < 21 and now.weekday() < 5:
         print("Running job at:", now)
-        subprocess.run(["python", "/Users/FreddieLewin/PycharmProjects/pythonProject19/alpaca_trading_api.py"])
+        subprocess.run(["python", "/Users/FreddieLewin/PycharmProjects/new_dl_token/algo_new/execute_trades_latest.py"])
+
 
 if __name__ == '__main__':
-    schedule.every(20).minutes.do(job) # 5 minutes since the count-down is from the finish of the code which takes 25
+    job()
+    schedule.every(12).minutes.do(job)
     while True:
         schedule.run_pending()
         time.sleep(1)
